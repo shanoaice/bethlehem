914611fae7013a9026695c141dad7bfe72545b50
KsRyY <andy200511@126.com>
2020-01-28 15:53:38 +0800

    :pencil2: Fix typo.

--- src/join.ts
+++ src/join.ts
@@ -1,35 +1,35 @@
 /**
  * Join elements in an array together with a specified seperator (curried)
  * ```js
- * join([1, 2, 3])('-') //=> '1-2-3'
- * join(['la', 'laa', 'lala])('-') //=> 'la-laa-lala'
+ * join('-')([1, 2, 3]) //=> '1-2-3'
+ * join('-')(['la', 'laa', 'lala']) //=> 'la-laa-lala'
  * ```
  */
-function join(list: Array<number | string>): (seperator: string) => string
+function join(seperator: string): (list: Array<number | string>) => string
 /**
  * Join elements in an array together with a specified seperator (curried)
  * ```js
- * join([1, 2, 3], '-') //=> '1-2-3'
- * join(['la', 'laa', 'lala], '-') //=> 'la-laa-lala'
+ * join('-', [1, 2, 3]) //=> '1-2-3'
+ * join('-', ['la', 'laa', 'lala']) //=> 'la-laa-lala'
  * ```
  */
-function join(list: Array<number | string>, seperator: string): string
+function join(seperator: string, list: Array<number | string>): string
 function join(
-  list: Array<number | string>,
-  seperator?: string
-): string | ((seperator: string) => string) {
-  if (typeof seperator !== undefined) {
+  seperator: string,
+  list?: Array<number | string>
+): string | ((list: Array<number | string>) => string) {
+  if (typeof list !== 'undefined') {
     let result = ''
 
-    // eslint-disable-next-line unicorn/no-for-loop, @typescript-eslint/prefer-for-of
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of, unicorn/no-for-loop
     for (let i = 0; i < list.length; i++) {
-      result += `${list[i]}${seperator as string}`
+      result += `${list[i]}${seperator}`
     }
 
     return result.slice(0, -1)
   }
 
-  return seperator => join(list, seperator)
+  return list => join(seperator, list)
 }
 
 export default join
