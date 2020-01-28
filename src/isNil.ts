a3948e98b3bd5060fa8a298018acdd8311cef58c
KsRyY <andy200511@126.com>
2020-01-28 17:30:04 +0800

    :pencil2: Correctly name the exported function in isNil.ts

--- src/isNil.ts
+++ src/isNil.ts
@@ -1,14 +1,14 @@
 /**
  * Test if a value is `null` or `undefined`
  * ```js
- * isEmpty(null) //=> true
- * isEmpty(undefined) //=> true
- * isEmpty(0) //=> false
- * isEmpty(false) //=> false
- * isEmpty(1) //=> false
+ * isNil(null) //=> true
+ * isNil(undefined) //=> true
+ * isNil(0) //=> false
+ * isNil(false) //=> false
+ * isNil(1) //=> false
  * ```
  */
-const isEmpty = (operand: any): boolean =>
+const isNil = (operand: any): boolean =>
   operand === null || operand === undefined
 
-export default isEmpty
+export default isNil
