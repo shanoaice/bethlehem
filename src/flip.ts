85ccda73f7e3ffa7fdc27b521a44ab095bf4b081
KsRyY <andy200511@126.com>
2020-01-29 09:58:38 +0800

    :sparkles: Add a new B.flip that works like R.flip

--- src/flip.ts
+++ src/flip.ts
@@ -1,21 +1,15 @@
-import reverse from './reverse'
-
 /**
- * Returns a new function much like the supplied one, except that **all** the arguments' order is reversed. **Warning: unlike Rambda, this function will flip app the arguments, not only the first two.**
+ * Returns a new function much like the supplied one, except that the first two arguments' order is reversed.
  * ```js
  * const mergeThree = (a, b, c) => [].concat(a, b, c);
  * mergeThree(1, 2, 3); //=> [1, 2, 3]
- * flip(mergeThree)(1, 2, 3); //=> [3, 2, 1]
+ * flip(mergeThree)(1, 2, 3); //=> [2, 1, 3]
  * ```
  */
-const flip = function<Arguments, Returns>(
-  func: (...args: Arguments[]) => Returns
-): (...args: Arguments[]) => Returns {
-  return (...args) => {
-    const reversedArgs = reverse(args)
-
-    return func(...reversedArgs)
-  }
+const flipAll = function<ArgumentOne, ArgumentTwo, Arguments, Returns>(
+  func: (arg1: ArgumentOne, arg2: ArgumentTwo, ...rest: Arguments[]) => Returns
+): (arg1: ArgumentTwo, arg2: ArgumentOne, ...rest: Arguments[]) => Returns {
+  return (arg1, arg2, ...args) => func(arg2, arg1, ...args)
 }
 
-export default flip
+export default flipAll
