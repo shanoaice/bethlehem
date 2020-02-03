d5c3326bb1f1e4b3dde9f2d1329f584d68037515
KsRyY <andy200511@126.com>
2020-02-03 17:02:14 +0800

    :bulb: Add TypeDoc comments for B.iterate

--- src/iterate.ts
+++ src/iterate.ts
@@ -2,15 +2,30 @@ import toString from './toString'
 import functionalSwitch from './functionalSwitch'
 import objectIterator from './helpers/objectIterator'
 
+/**
+ * @ignore
+ */
 const isSet = (x: any): boolean =>
   toString(x) === '[object Set]' || toString(x) === '[object WeakSet]'
 
+/**
+ * @ignore
+ */
 const isString = (x: any): boolean => toString(x) === '[object String]'
 
+/**
+ * @ignore
+ */
 const isArray = (x: any): boolean => Array.isArray(x)
 
+/**
+ * @ignore
+ */
 const isObject = (x: any): boolean => toString(x) === '[object Object]'
 
+/**
+ * @ignore
+ */
 const isMap = (x: any): boolean =>
   toString(x) === '[object Map]' || toString(x) === '[object WeakMap]'
 
@@ -52,39 +67,201 @@ type iterateCallbackWithWeakSet<T extends object> = (
   set: WeakSet<T>
 ) => undefined
 
+/**
+ * Iterate through all the enumerable key-value pairs of the object and apply it to the callback. (curried)
+ * ```js
+ * const test = { a: 1, b: 2 }
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // a: 1
+ * // b: 2
+ * ```
+ */
 function iterate<T extends object>(
   cb: iterateCallbackWithObject<T>
 ): (object: T) => T
+/**
+ * Iterate through all the entries of the map and apply it to the callback. (curried)
+ * ```js
+ * const test = new Map(Object.entries({ a: 1, b: 2 }))
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // a: 1
+ * // b: 2
+ * ```
+ */
 function iterate<Key, Value>(
   cb: iterateCallbackWithMap<Key, Value>
 ): (map: Map<Key, Value>) => Map<Key, Value>
+/**
+ * Iterate through all the entries of the weak map and apply it to the callback. (curried)
+ * ```js
+ * const weakMapKeyOne = { a: 1 }
+ * const weakMapKeyTwo = { b: 2 }
+ * const test = new WeakMap([[weakMapKeyOne, 1], [weakMapKeyTwo, 2]])
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // { a: 1 }: 1
+ * // { b: 2 }: 2
+ * ```
+ */
 function iterate<Key extends object, Value>(
   cb: iterateCallbackWithWeakMap<Key, Value>
 ): (map: WeakMap<Key, Value>) => WeakMap<Key, Value>
+/**
+ * Iterate through all the entries of the set and apply it to the callback. (curried)
+ * ```js
+ * const test = new Set([1, 2])
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // 1
+ * // 2
+ * ```
+ */
 function iterate<T>(cb: iterateCallbackWithSet<T>): (set: Set<T>) => Set<T>
+/**
+ * Iterate through all the entries of the weak set and apply it to the callback. (curried)
+ * ```js
+ * const weakSetEntryOne = { a: 1 }
+ * const weakSetEntryTwo = { b: 2 }
+ * const test = new WeakSet([weakSetEntryOne, weakSetEntryTwo])
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // { a: 1 }
+ * // { b: 2 }
+ * ```
+ */
 function iterate<T extends object>(
   cb: iterateCallbackWithWeakSet<T>
 ): (set: WeakSet<T>) => WeakSet<T>
+/**
+ * Iterate through all the elements inside the array and apply it to the callback. (curried)
+ * ```js
+ * const test = [1, 2]
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // 1
+ * // 2
+ * ```
+ */
 function iterate<T>(cb: iterateCallbackWithArray<T>): (array: T[]) => T[]
+/**
+ * Iterate through all the chars of the string and apply it to the callback. (curried)
+ * ```js
+ * const test = 'ab'
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // a
+ * // b
+ * ```
+ */
 function iterate(cb: iterateCallbackWithString): (string: string) => string
+/**
+ * Iterate through all the enumerable key-value pairs of the object and apply it to the callback.
+ * ```js
+ * const test = { a: 1, b: 2 }
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // a: 1
+ * // b: 2
+ * ```
+ */
 function iterate<T extends object>(
   cb: iterateCallbackWithObject<T>,
   object: T
 ): T
+/**
+ * Iterate through all the entries of the map and apply it to the callback.
+ * ```js
+ * const test = new Map(Object.entries({ a: 1, b: 2 }))
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // a: 1
+ * // b: 2
+ * ```
+ */
 function iterate<Key, Value>(
   cb: iterateCallbackWithMap<Key, Value>,
   map: Map<Key, Value>
 ): Map<Key, Value>
+/**
+ * Iterate through all the entries of the weak map and apply it to the callback.
+ * ```js
+ * const weakMapKeyOne = { a: 1 }
+ * const weakMapKeyTwo = { b: 2 }
+ * const test = new WeakMap([[weakMapKeyOne, 1], [weakMapKeyTwo, 2]])
+ * const cb = (val, key) => console.log(`${key}: ${val}`)
+ * iterate(cb, test)
+ * // console output
+ * // { a: 1 }: 1
+ * // { b: 2 }: 2
+ * ```
+ */
 function iterate<Key extends object, Value>(
   cb: iterateCallbackWithWeakMap<Key, Value>,
   map: WeakMap<Key, Value>
 ): WeakMap<Key, Value>
+/**
+ * Iterate through all the entries of the set and apply it to the callback.
+ * ```js
+ * const test = new Set([1, 2])
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // 1
+ * // 2
+ * ```
+ */
 function iterate<T>(cb: iterateCallbackWithSet<T>, set: Set<T>): Set<T>
+/**
+ * Iterate through all the entries of the weak set and apply it to the callback.
+ * ```js
+ * const weakSetEntryOne = { a: 1 }
+ * const weakSetEntryTwo = { b: 2 }
+ * const test = new WeakSet([weakSetEntryOne, weakSetEntryTwo])
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // { a: 1 }
+ * // { b: 2 }
+ * ```
+ */
 function iterate<T extends object>(
   cb: iterateCallbackWithWeakSet<T>,
   set: WeakSet<T>
 ): WeakSet<T>
+/**
+ * Iterate through all the elements inside the array and apply it to the callback.
+ * ```js
+ * const test = [1, 2]
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // 1
+ * // 2
+ * ```
+ */
 function iterate<T>(cb: iterateCallbackWithArray<T>, array: T[]): T[]
+/**
+ * Iterate through all the chars of the string and apply it to the callback.
+ * ```js
+ * const test = 'ab'
+ * const cb = (val) => console.log(val)
+ * iterate(cb, test)
+ * // console output
+ * // a
+ * // b
+ * ```
+ */
 function iterate(cb: iterateCallbackWithString, string: string): string
 function iterate<
   ObjectT extends object,
@@ -161,7 +338,10 @@ function iterate<
       {
         condition: isObject,
         task: object => {
-          object[Symbol.iterator] = objectIterator
+          if (!object[Symbol.iterator]) {
+            object[Symbol.iterator] = objectIterator
+          }
+
           for (const [key, value] of object) {
             cb(value, key, object)
           }
